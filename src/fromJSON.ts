2784528e6b55031b81a4371d393e56d432c9e2d8
ihor <ihor.bielchenko@gmail.com>
2025-05-07 19:36:29 +0300

    Update fromJSON function.

--- src/fromJSON.ts
+++ src/fromJSON.ts
@@ -1,9 +1,37 @@
 
 export function fromJSON(value) {
 	try {
+		let key = ``,
+			parsed: any = {};
+
+		if (isArrFilled(value)) {
+			parsed = [];
+
+			Array
+				.from(value)
+				.forEach((item) => {
+					parsed.push(JSON.parse(item));
+				});
+			return parsed;
+		}
+		else if (isObjFilled(value)) {
+			for (key in value) {
+				parsed[key] = JSON.parse(value[key]);
+			}
+			return parsed;
+		}
 		return JSON.parse(value);
 	}
 	catch (err) {
+	}
+	if (isStrFilled(value) 
+		&& ((value[0] === `'`
+			&& value[value.length - 1] === `'`)
+		|| (value[0] === `"`
+			&& value[value.length - 1] === `"`)
+		|| (value[0] === '`'
+			&& value[value.length - 1] === '`'))) {
+		return value.slice(1).slice(0, value.length - 2);
 	}
 	return null;
 }
