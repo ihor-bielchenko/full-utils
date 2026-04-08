566996e156aa4fad09adf6ab72bd8cf036634e4b
ihor <ihor.bielchenko@gmail.com>
2026-04-08 07:31:04 +0300

    Fix urlDecode.

--- src/url/urlDecode.ts
+++ src/url/urlDecode.ts
@@ -6,7 +6,7 @@ export function urlDecode(input: string): string {
 	}
 	const RAW = '____RAW_PERCENT____';
 	const pre = input.replace(/%(?![0-9A-Fa-f]{2})/g, RAW);
-	const WHITELIST = new Set(['22', '7B', '7D', '3A', '2C', '5B', '5D', '5C']);
+	const WHITELIST = new Set(['20', '22', '7B', '7D', '3A', '2C', '5B', '5D', '5C']);
 	const decoded = pre.replace(/%([0-9A-Fa-f]{2})/g, (_m, hh: string) => {
 		const code = hh.toUpperCase();
 
